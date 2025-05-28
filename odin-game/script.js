718c560d7ac079baaf674fb4486c147504df719f
lavs123 <lavilsonc4pires@gmail.com>
2025-05-28 16:16:17 -0100

    Setting up the functions

--- odin-game/script.js
+++ odin-game/script.js
@@ -1,1 +1,21 @@
-console.log("hello world");
+
+/**on this game cpChoice means  = computer choice
+on this game hunChoice means  = human choice */
+
+//GAME SCORE
+let humanScore = 0;
+let computerScore =0;
+
+function getComputerChoice ( ){
+   const cpChoice = ["rock", "paper", "scissors"];
+   
+   let chosed = cpChoice[Math.floor(choice.length * Math.random())];
+
+    return chosed;
+}
+
+function getHumanChoice(){
+    let hunChoice = prompt("Type your choice rock, paper or scissors. :");
+
+    return hunChoice;
+}
